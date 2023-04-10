c976fe6d029d4b258b4d8a23cc4b3c068c870acd
wilvers <juan.ignacio.wilvers@gmail.com>
2023-04-10 18:47:23 -0300

    route login

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -5,6 +5,7 @@ export const AppRouter = ()=>{
     return(
         <Routes>
         <Route path="/home" Component={<HomePage />}/>
+        <Route path="/login" Component={<LoginPage />} />
         </Routes>
     )
 }
