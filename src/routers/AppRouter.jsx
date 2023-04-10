56ce8661c29474eb8154ee0580fda537fe74a08b
wilvers <juan.ignacio.wilvers@gmail.com>
2023-04-10 19:08:03 -0300

    init LoginPage

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import { Route, Routes } from "react-router"
 import { HomePage } from "../pages/HomePage"
+import { LoginPage } from "../pages/LoginPage"
 
 
 export const AppRouter = ()=>{
     return(
         <Routes>
         <Route path="/home" element={<HomePage />}/>
-       {/* // <Route path="/login" Component={<LoginPage />} /> */}
+        <Route path="/login" element={<LoginPage />} /> 
         </Routes>
     )
 }
