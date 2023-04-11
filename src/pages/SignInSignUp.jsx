8a77f127565a49ab39a52d838b706346c665f29b
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-11 16:05:24 -0300

    cambio de componente

--- src/pages/SignInSignUp.jsx
+++ src/pages/SignInSignUp.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const LoginPage =()=>{
+export const SignInSignUp =()=>{
     return(
         <h1>login</h1>
     )
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,14 +1,14 @@
 import React from "react"
 import { Route, Routes } from "react-router"
 import { HomePage } from "../pages/HomePage"
-import { LoginPage } from "../pages/LoginPage"
+import { SignInSignUp } from "../pages/SignInSignUp"
 
 
 export const AppRouter = ()=>{
     return(
         <Routes>
         <Route path="/home" element={<HomePage />}/>
-        <Route path="/login" element={<LoginPage />} /> 
+        <Route path="/login" element={<SignInSignUp />} /> 
         </Routes>
     )
 }
