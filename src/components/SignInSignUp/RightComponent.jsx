ff097f6ee9b74fd95f7328262d53017f3e656005
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-11 16:16:31 -0300

    RightComponent

---
+++ src/components/SignInSignUp/RightComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Col } from 'react-bootstrap'
+
+export const RightComponent = () => {
+    return (
+        <Col className='signin-signup__left'>
+            <h2>Right</h2>
+        </Col>
+    )
+}
