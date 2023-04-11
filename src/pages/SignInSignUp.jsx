b552c937d6f189faab6612ff684b249ecb468b18
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-11 16:22:31 -0300

    update SignInSignUp

--- src/pages/SignInSignUp.jsx
+++ src/pages/SignInSignUp.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Container, Row } from "react-bootstrap";
+import { LeftComponent } from "../components/SignInSignUp/LeftComponent";
+import { RightComponent } from "../components/SignInSignUp/RightComponent";
 
-export const SignInSignUp =()=>{
-    return(
-        <h1>login</h1>
+export const SignInSignUp = () => {
+    return (
+        <Container className="signin-signup" fluid>
+            <Row>
+                <LeftComponent />
+                <RightComponent />
+            </Row>
+        </Container>
     )
 }
