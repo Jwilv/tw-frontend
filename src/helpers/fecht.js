b3d1a22f1afc4560abf397d61b839f462c3a1ad5
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-13 16:34:11 -0300

    fetchWithoToken

---
+++ src/helpers/fecht.js
@@ -0,0 +1,22 @@
+
+const baseUrl = ""
+
+export const fetchWithoToken = async (endpoint, data, method = "GET") => {
+    const url = `${baseUrl}/${endpoint}`
+
+    if (method === "GET") {
+        const resp = await fetch(url)
+        return await resp.json()
+    } else {
+        const resp = await fetch(url, {
+            method: method,
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+
+        return await resp.json()
+    }
+
+}
