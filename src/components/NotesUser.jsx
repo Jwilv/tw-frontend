bae0eaa584947b678f7e628fa7e4afb3bb204718
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-19 19:11:56 -0300

    update styles

--- src/components/NotesUser.jsx
+++ src/components/NotesUser.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 export const NotesUser = () => {
     return (
-        <div>
+        <div className='notes-user-container'>
+
+            <div className='notes-user-img'>
+                <img
+                    src="https://tse2.mm.bing.net/th?id=OIP.eAevlGw1UR_UqXA_2y-2tAHaHa&pid=Api&P=0"
+                    alt="img user"
+                    className='img-user'
+                />
+            </div>
             <h1>NotesUser</h1>
             <hr />
         </div>
--- src/pages/MenuNotes.jsx
+++ src/pages/MenuNotes.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { NotesUser } from '../components/NotesUser'
 
 export const MenuNotes = () => {
     return (
         <div className='MenuNotes-container'>
             <NotesUser />
+            <NotesUser />
+            <NotesUser />
+            <NotesUser />
+            <NotesUser />
+            <NotesUser />
+            <NotesUser />
+            <NotesUser />
+            <NotesUser />
+            <NotesUser />
+            <NotesUser />
+            <NotesUser />
+            <NotesUser />
+            <NotesUser />
+            <NotesUser />
+
         </div>
     )
 }
