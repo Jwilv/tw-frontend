88aa5111fc33e3c509a7c83f048425312df700a4
wilvers <juan.ignacio.wilvers@gmail.com>
2023-04-20 20:39:05 -0300

    init Post-notes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import { BasicLayout } from "../components/BasicLayout"
 import { MenuNotes } from "./MenuNotes"
+import { PostNote } from "../components/PostNote"
 
 export const HomePage = () => {
     return (
         <BasicLayout >
+            <PostNote />
             <MenuNotes />
         </BasicLayout>
     )
