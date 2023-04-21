a7496b0a44896dafac009c3922fa65e53ecc9d02
wilvers <juan.ignacio.wilvers@gmail.com>
2023-04-21 10:40:06 -0300

    update styles menu right

--- src/components/MenuRight.jsx
+++ src/components/MenuRight.jsx
@@ -4,6 +4,8 @@ export const MenuRight = () => {
     return (
         <div className='menu-container-right'>
             <div className='menu-container-right-items'>hoolaaa</div>
+            <div className='menu-container-right-items'>chau</div>
+
         </div>
     )
 }
