9ca360ec0ff87d74b369410db68437c27d4c6ba3
wilvers <juan.ignacio.wilvers@gmail.com>
2023-04-21 10:45:37 -0300

    init UserRecommended

---
+++ src/components/UserRecommended.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const UserRecommended = () => {
+    return (
+        <div className='menu-right-user'>
+            <div className="menu-right-user-img">
+                <img
+                    src="https://tse2.mm.bing.net/th?id=OIP.eAevlGw1UR_UqXA_2y-2tAHaHa&pid=Api&P=0"
+                    alt="img user"
+                    className='img-user'
+                />
+            </div>
+        </div>
+    )
+}
