384940247739fe16ebba1d44ebc5f13d9cfeacb7
wilvers <juan.ignacio.wilvers@gmail.com>
2023-04-21 11:36:51 -0300

    update menu right notes

--- src/components/MenuRight.jsx
+++ src/components/MenuRight.jsx
@@ -5,14 +5,22 @@ import { UserRecommended } from './UserRecommended'
 export const MenuRight = () => {
     return (
         <div className='menu-container-right'>
-            <div className='menu-container-right-items'>
+            <div className='menu-container-right-items-user'>
+                <p className="menu-container-right-title"> Usuarios recomendados: </p>
+                <UserRecommended />
+                <UserRecommended />
+                <UserRecommended />
                 <UserRecommended />
             </div>
 
             <div className='menu-container-right-items'>
-                <NotesUser />
-                <NotesUser />
-                <NotesUser />
+                <p className="menu-container-right-title-notes"> Publicaciones recomendados: </p>
+                <div className='menu-container-right-notes'>
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                </div>
+
             </div>
 
         </div>
