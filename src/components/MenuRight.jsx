0ecd6c39305d4ff98f201ad579a3319c9795a2e5
wilvers <juan.ignacio.wilvers@gmail.com>
2023-04-21 10:53:14 -0300

    notas recomendadas

--- src/components/MenuRight.jsx
+++ src/components/MenuRight.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { NotesUser } from './NotesUser'
 
 export const MenuRight = () => {
     return (
         <div className='menu-container-right'>
             <div className='menu-container-right-items'>hoolaaa</div>
-            <div className='menu-container-right-items'>chau</div>
+            <div className='menu-container-right-items'>
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+            </div>
 
         </div>
     )
