091d037d29728b4a5eaf7f9a6358f5987f18cd7f
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-20 23:39:03 -0300

    icons

--- src/components/BasicLayout.jsx
+++ src/components/BasicLayout.jsx
@@ -1,13 +1,39 @@
+
 import React from 'react'
 export const BasicLayout = ({ children, className }) => {
     return (
         <div className='menu-container'>
             <div className='menu-left'>
                 <div className='menu-left-options'>
-                    <p className='menu-left-items'>inicio</p>
-                    <p className='menu-left-items'>Buscar Personas</p>
-                    <p className='menu-left-items'>Perfil</p>
-                    <p className='menu-left-items'>Logout</p>
+
+                    <p className='menu-left-items'>
+                        <i className="fa-solid fa-house"
+                            style={{ color: "#ffffff", }}
+                        />
+                        inicio
+                    </p>
+
+                    <p className='menu-left-items'>
+                        <i className="fa-solid fa-magnifying-glass"
+                            style={{ color: "#ffffff", }}
+                        />
+                        Buscar Personas
+                    </p>
+
+                    <p className='menu-left-items'>
+                        <i className="fa-solid fa-user"
+                            style={{ color: "#ffffff", }}
+                        />
+                        Perfil
+                    </p>
+
+                    <p className='menu-left-items'>
+                        <i className="fa-solid fa-power-off"
+                            style={{ color: "#ffffff", }}
+                        />
+                        Logout
+                    </p>
+                    
                 </div>
             </div>
             <div>
