e0b3c330e95761ae8c5e516772675c4794583656
wilvers <juan.ignacio.wilvers@gmail.com>
2023-04-20 22:49:12 -0300

    update basic

--- src/components/BasicLayout.jsx
+++ src/components/BasicLayout.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 export const BasicLayout = ({ children, className }) => {
     return (
         <div className='menu-container'>
-            <div className='menu-left'>menu</div>
+            <div className='menu-left'>
+                <div className='menu-left-options'>
+                    <p className='menu-left-items'>inicio</p>
+                    <p className='menu-left-items'>Buscar Personas</p>
+                    <p className='menu-left-items'>Perfil</p>
+                    <p className='menu-left-items'>Logout</p>
+                </div>
+            </div>
             <div>
                 {children}
             </div>
