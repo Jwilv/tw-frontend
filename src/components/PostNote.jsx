87cd707c73ffac1ee736d170056c93b336328c8e
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-21 16:26:57 -0300

    init card post note

--- src/components/PostNote.jsx
+++ src/components/PostNote.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 export const PostNote = () => {
   return (
     <div className='container-post-note'>
-        
+      <div className="card-post-note">
+        <div className="user-post">
+          <div className="user-post-img">
+            <img
+              src="https://tse2.mm.bing.net/th?id=OIP.eAevlGw1UR_UqXA_2y-2tAHaHa&pid=Api&P=0"
+              alt="img user"
+              className='user-post-img-user'
+            />
+          </div>
+          <div className="user-post-name">
+            <p>juan carlos chupa pija</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
