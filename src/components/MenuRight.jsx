7e6dbb78a61a3e2353363a6cc04322f9ee30b8c9
wilvers <juan.ignacio.wilvers@gmail.com>
2023-04-21 09:33:33 -0300

    update right items

--- src/components/MenuRight.jsx
+++ src/components/MenuRight.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 export const MenuRight = () => {
-  return (
-    <div>MenuRight</div>
-  )
+    return (
+        <div className='menu-container-right'>
+            <div className='menu-container-right-items'>hoolaaa</div>
+        </div>
+    )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,17 @@ import React from "react"
 import { BasicLayout } from "../components/BasicLayout"
 import { MenuNotes } from "./MenuNotes"
 import { PostNote } from "../components/PostNote"
+import { MenuRight } from "../components/MenuRight"
 
 export const HomePage = () => {
     return (
-        <BasicLayout >
-            <PostNote />
-            <MenuNotes />
-        </BasicLayout>
+        <div className="menu-container">
+            <BasicLayout >
+                <PostNote />
+                <MenuNotes />
+            </BasicLayout>
+            <MenuRight />
+        </div>
+
     )
 }
