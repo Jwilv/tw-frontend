55b33101fdda5eb4cc8937be097f14f381cbc2df
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-21 23:19:59 -0300

    new PostNote

--- src/components/PostNote.jsx
+++ src/components/PostNote.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 
 export const PostNote = () => {
-  return (<h1>g</h1>
+  return (
+    <div className="post-note-container">
+      <div className="post-note-user">
+        <img
+          src="https://tse2.mm.bing.net/th?id=OIP.eAevlGw1UR_UqXA_2y-2tAHaHa&pid=Api&P=0"
+          alt="img user"
+        />
+        <p>name</p>
+      </div>
+      <div className="post-note-input">
+        <input type="text" placeholder='queee soo ?' />
+        <button>Enviar</button>
+      </div>
+    </div>
   )
 }
