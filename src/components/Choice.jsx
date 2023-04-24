a0e12c1fa11ecc90b3cf01247eb35676911a4d09
wilvers <juan.ignacio.wilvers@gmail.com>
2023-04-24 19:46:52 -0300

    Choice component

---
+++ src/components/Choice.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Choice = () => {
+  return (
+    <div className='choice-container'>
+      <div className="choice-home">home</div>
+      <div className="choice-suggestions">Suggestions</div>
+    </div>
+  )
+}
--- src/pages/MenuNotes.jsx
+++ src/pages/MenuNotes.jsx
@@ -1,30 +1,32 @@
 import React from 'react'
 import { NotesUser } from '../components/NotesUser'
 import { PostNote } from '../components/PostNote'
+import { Choice } from '../components/Choice'
 
 export const MenuNotes = () => {
     return (
-        <div className='MenuNotes-container'>
-            <PostNote />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-            <NotesUser />
-
-
-        </div>
+        <>
+            <Choice />
+            <div className='MenuNotes-container'>
+                <PostNote />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+            </div>
+        </>
     )
 }
