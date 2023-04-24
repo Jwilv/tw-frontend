5d65b31a1c1e0dae8d278aa73fd84d3e74e335a6
wilvers <juan.ignacio.wilvers@gmail.com>
2023-04-24 20:05:08 -0300

    uiReducer en store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth.slice"
+import uiReducer from "./ui.slice"
 
 
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        ui : uiReducer,
     }
 })
