72bcfd014e22f9d1c30496a2c53a44e1f7633c3a
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-25 10:29:40 -0300

    active and not-active

--- src/components/Choice.jsx
+++ src/components/Choice.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { openMenu, openSuggestions } from '../redux/ui.slice';
 
 export const Choice = () => {
+  const dispatch = useDispatch();
+
+  const { suggestions, home}  = useSelector( state => state.ui)
+
+  const handleHome = () => {
+    dispatch(openMenu());
+  }
+
+  const handleSuggestions = () => {
+    dispatch(openSuggestions())
+  }
+
   return (
     <div className='choice-container'>
-      <div className="choice-home">home</div>
-      <div className="choice-suggestions">Suggestions</div>
+      <div
+        className="choice-home"
+        onClick={handleHome}
+      >
+        <p className={home ? "active" : "not-active"}>home</p>
+      </div>
+      <div
+        className="choice-suggestions"
+        onClick={handleSuggestions}
+      >
+        <p className={suggestions ? "active" : "not-active"}>Suggestions</p>
+      </div>
     </div>
   )
+
 }
