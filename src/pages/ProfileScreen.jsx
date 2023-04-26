0a82fb80dbdd1e5d05cdb91382bee4ca065f1c0a
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-26 16:05:06 -0300

    params

--- src/pages/ProfileScreen.jsx
+++ src/pages/ProfileScreen.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router'
 
 export const ProfileScreen = () => {
+    const {profileId} = useParams();
+    console.log(profileId)
     return (
-        <div>ProfileScreen</div>
+        <>
+        <ProfileInfo /> 
+        </>
     )
 }
