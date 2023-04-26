e4caf3e93a3af4a57a6d8328bb1939023a2ad7cf
wilvers <juan.ignacio.wilvers@gmail.com>
2023-04-26 20:58:57 -0300

    update styles ProfileInfo

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -5,8 +5,34 @@ export const ProfileInfo = () => {
         <div className='container-profileinfo'>
             <div className="profileinfo-user">
                 <div className="banner">
+                    <img src="https://th.bing.com/th/id/OIP.4xEuR3oDwGgqu4jQqZ8FMgHaCq?w=316&h=125&c=7&r=0&o=5&pid=1.7"
+                        alt="banner"
+                        className='banner-img'
+                    />
+                </div>
+
+                <div className="info">
+
+                    <div className="img-profile">
+                        <img src="https://th.bing.com/th/id/OIP.sCeg7W9TuHWg3r6uxJztlAHaHa?w=202&h=202&c=7&r=0&o=5&pid=1.7"
+                            alt="avatar"
+                        />
+                    </div>
+
+                    <div className="container-bio-name">
+
+                    <div className="name-profile">
+                        <p>jajaajajjaja</p>
+                    </div>
+
+                    <div className="bio-profile">
+                        <p>jajajajaajajajjaggggg</p>
+                    </div>
+
+                    </div>
 
                 </div>
+
             </div>
 
         </div>
