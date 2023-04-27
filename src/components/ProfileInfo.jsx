8a795ce38da34a962ccd6f8fb6e88721c948c10f
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-26 22:36:03 -0300

    ProfileInfo styles

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -5,7 +5,7 @@ export const ProfileInfo = () => {
         <div className='container-profileinfo'>
             <div className="profileinfo-user">
                 <div className="banner">
-                    <img src="https://th.bing.com/th/id/OIP.4xEuR3oDwGgqu4jQqZ8FMgHaCq?w=316&h=125&c=7&r=0&o=5&pid=1.7"
+                    <img src="https://tse3.mm.bing.net/th?id=OIP.MpAoLVf4y8Dt8v5ZDcyu3QHaCp&pid=Api&P=0"
                         alt="banner"
                         className='banner-img'
                     />
@@ -14,20 +14,23 @@ export const ProfileInfo = () => {
                 <div className="info">
 
                     <div className="img-profile">
-                        <img src="https://th.bing.com/th/id/OIP.sCeg7W9TuHWg3r6uxJztlAHaHa?w=202&h=202&c=7&r=0&o=5&pid=1.7"
+                        <img src="https://tse2.mm.bing.net/th?id=OIP.KrwtXzJJ9VXkvOX59pIEeQHaHi&pid=Api&P=0"
                             alt="avatar"
                         />
                     </div>
 
                     <div className="container-bio-name">
 
-                    <div className="name-profile">
-                        <p>jajaajajjaja</p>
-                    </div>
+                        <div className="name-profile">
+                            <strong>pedro</strong>
+                            <strong>faustino tercero</strong>
+                            <p> <i className="fa-solid fa-location-dot" style={{color: "#ffffff",}} />Las piernas de tu mujer</p>
+                        </div>
 
-                    <div className="bio-profile">
-                        <p>jajajajaajajajjaggggg</p>
-                    </div>
+                        <div className="bio-profile">
+                            <p className='date'><i className="fa-solid fa-calendar" style={{color: "#ffffff",}} />20/03/2003</p>
+                            <p>me gustan las peliculas y los jueguitos de pc, me dedico a insultar a las madres de mis compañeros de partida y irme afk</p>
+                        </div>
 
                     </div>
 
