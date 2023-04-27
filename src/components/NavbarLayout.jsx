26be2eb2fc7b99b76155480182d370fe54f481e9
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-27 15:53:23 -0300

    backPage

--- src/components/NavbarLayout.jsx
+++ src/components/NavbarLayout.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useNavigate, useNavigation } from 'react-router'
+import { unstable_HistoryRouter } from 'react-router-dom';
 
-export const NavbarLayout = ({children}) => {
+export const NavbarLayout = ({ children }) => {
+
+
+    const navigate  = useNavigate()
+
+    const PREVIOUS_PAGE = -1
+
+    const backPage = ()=>{
+        navigate(PREVIOUS_PAGE)
+    }
+
     return (
         <>
-        <h1>NavbarLayout</h1>
-        {children}
+            <h1
+                onClick={backPage}>NavbarLayout</h1>
+            {children}
         </>
     )
 }
