fb78ecbaa87462ed496fcd358398c4d878edcc8a
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-27 14:27:05 -0300

    notes in  profile

--- src/pages/ProfileScreen.jsx
+++ src/pages/ProfileScreen.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import { useParams } from 'react-router'
 import { ProfileInfo } from '../components/ProfileInfo';
+import { PostNote } from '../components/PostNote';
+import { NotesUser } from '../components/NotesUser';
 
 export const ProfileScreen = () => {
-    const {profileId} = useParams();
+    const { profileId } = useParams();
     console.log(profileId)
     return (
         <>
-        <ProfileInfo /> 
+            <ProfileInfo />
+            <div className='MenuNotes-container'>
+                <PostNote />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+                <NotesUser />
+            </div>
         </>
     )
 }
