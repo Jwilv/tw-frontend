1b9b8eeab2e14f3b5e134cea774de73dab88fed1
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-27 15:15:41 -0300

    ProfileScreen refactor

---
+++ src/components/Profile.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { NotesUser } from './NotesUser'
+import { PostNote } from './PostNote'
+import { ProfileInfo } from './ProfileInfo'
+
+export const Profile = () => {
+    return (
+        <div className='profile-container'>
+            <ProfileInfo />
+            <div className="notes-profile">
+                <div className='notes-profile-width'>
+                    <PostNote />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                    <NotesUser />
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/ProfileScreen.jsx
+++ src/pages/ProfileScreen.jsx
@@ -1,54 +1,14 @@
 import React from 'react'
 import { useParams } from 'react-router'
-import { ProfileInfo } from '../components/ProfileInfo';
-import { PostNote } from '../components/PostNote';
-import { NotesUser } from '../components/NotesUser';
+import { Profile } from '../components/Profile';
+import { NavbarLayout } from '../components/NavbarLayout';
 
 export const ProfileScreen = () => {
     const { profileId } = useParams();
     console.log(profileId)
     return (
-        <div className='profile-container'>
-            <ProfileInfo />
-            <div className="notes-profile">
-                <div className='notes-profile-width'>
-                    <PostNote />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                    <NotesUser />
-                </div>
-            </div>
-        </div>
+        <NavbarLayout >
+            <Profile />
+        </NavbarLayout>
     )
 }
