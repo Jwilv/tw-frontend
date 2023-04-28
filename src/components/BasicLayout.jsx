b0da22a4c06d7bdd942e4d64504f072f0c3a73b9
wilvers <juan.ignacio.wilvers@gmail.com>
2023-04-28 08:22:25 -0300

    links menu

--- src/components/BasicLayout.jsx
+++ src/components/BasicLayout.jsx
@@ -4,10 +4,10 @@ import { useDispatch } from "react-redux"
 import { startLogout } from '../redux/auth.slice';
 import { Link } from 'react-router-dom';
 export const BasicLayout = ({ children, className }) => {
-    
+
     const dispatch = useDispatch();
 
-    const handleLogout = ()=>{
+    const handleLogout = () => {
         dispatch(startLogout())
     }
 
@@ -20,35 +20,41 @@ export const BasicLayout = ({ children, className }) => {
                         <i className="fa-solid fa-house"
                             style={{ color: "#ffffff", }}
                         />
-                        inicio
+                        <Link
+                            to={"/"}
+                            className='link'
+                        >Home</Link>
                     </p>
 
                     <p className='menu-left-items'>
                         <i className="fa-solid fa-magnifying-glass"
                             style={{ color: "#ffffff", }}
                         />
-                        Buscar Personas
+                        <Link
+                            to={"/search"}
+                            className='link'
+                        >Buscar Personas</Link>
                     </p>
 
                     <p className='menu-left-items'>
                         <i className="fa-solid fa-user"
                             style={{ color: "#ffffff", }}
                         />
-                        <Link 
-                        to={"/profile/:123"}
-                        className='link'
+                        <Link
+                            to={"/profile/:123"}
+                            className='link'
                         >Perfil</Link>
                     </p>
 
                     <p className='menu-left-items'
-                    onClick={handleLogout}
+                        onClick={handleLogout}
                     >
                         <i className="fa-solid fa-power-off"
                             style={{ color: "#ffffff", }}
                         />
                         Logout
                     </p>
-                    
+
                 </div>
             </div>
             <div>
