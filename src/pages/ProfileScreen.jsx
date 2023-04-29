e23584d2db849a48c63c119a08c251e75df2a696
wilvers <juan.ignacio.wilvers@gmail.com>
2023-04-29 18:26:25 -0300

    startDataProfile

--- src/pages/ProfileScreen.jsx
+++ src/pages/ProfileScreen.jsx
@@ -5,13 +5,8 @@ import { NavbarLayout } from '../components/NavbarLayout';
 import { getUserById } from '../helpers/getUserbyId';
 
 export const ProfileScreen = () => {
-    const { profileId } = useParams();
-    const resolver =async ()=>{
-        const res = await getUserById(profileId)
-        console.log( res)
-    }
 
-    resolver()
+    const { profileId } = useParams();
     
     return (
         <NavbarLayout >
---
+++ src/redux/profile.slice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit"
+import { getUserById } from "../helpers/getUserbyId"
+
+
+const initialState = {
+    name: "",
+    id: "",
+    location: "",
+    avatar: "",
+    banner: "",
+    birthDate: "",
+    surname: "",
+}
+
+const profileSlice = createSlice({
+    name: "profile",
+    initialState,
+    reducers: {
+        setProfileData: (state, action) => {
+            return {
+                ...state,
+                ...action.payload
+            }
+        },
+        deleteProfileData: () => {
+            return initialState
+        },
+
+    }
+})
+
+const { setProfileData, deleteProfileData } = profileSlice.actions;
+
+export const startDataProfile = (id) => {
+    return async (dispatch) => {
+        try {
+            const resp = await getUserById(id);
+            dispatch(setProfileData({...resp}))
+        } catch (error) {
+
+        }
+    }
+}
