303ace3b55c0c456d5bcbf7dca703071c7386168
wilvers <juan.ignacio.wilvers@gmail.com>
2023-05-01 18:59:12 -0300

    notesSlice

--- src/pages/ProfileScreen.jsx
+++ src/pages/ProfileScreen.jsx
@@ -13,6 +13,7 @@ export const ProfileScreen = () => {
 
     useEffect(() => {
       dispatch(startDataProfile(profileId))
+      
     }, [])
     
     
---
+++ src/redux/notes.slice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+const initialState = {
+    userNotes: [],
+    notes: [],
+    recommended: [],
+}
+
+const notesSlice = createSlice({
+    name: "notes",
+    initialState,
+    reducers: {
+        addNotes: (state, action) => {
+            return {
+                ...state,
+                ...action.payload
+            }
+        },
+        deleteNotes : ()=>{
+            return initialState
+        }
+    }
+})
+
