ceaf7cebb56577d845adf0cdaafe41a1a20ebbe5
wilvers <juan.ignacio.wilvers@gmail.com>
2023-05-03 16:41:05 -0300

    img user in post message

--- src/components/PostNote.jsx
+++ src/components/PostNote.jsx
@@ -1,31 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useForm } from '../hooks/useForm'
+import Swal from 'sweetalert2'
 
 export const PostNote = () => {
 
+  const {_id} = useSelector( state => state.user)
+
   const [ value, changevalue, reset] = useForm({message : ""})
+
+  const [Counter, setCounter] = useState(0)
 
   const {message} = value;
 
   const { name } = useSelector(state => state.user)
 
-  console.log(message)
+  const MAX_CHARACTERS = 250 ;
 
+  useEffect(() => {
+    setCounter( MAX_CHARACTERS - message.length)
+    console.log(Counter)
+  }, [message])
 
   const handlePostNote = (event) => {
     event.preventDefault()
+    if(message.length > 250){
+      return Swal.fire("Error","se permiten maximo 250 caracteres","error")
+    }
   }
   return (
     <div className="post-note-container">
       <div className="post-note-user">
         <img
-          src="https://tse2.mm.bing.net/th?id=OIP.eAevlGw1UR_UqXA_2y-2tAHaHa&pid=Api&P=0"
+          src={`http://localhost:8080/getAvatar?id=${_id}`}
           alt="img user"
         />
         <p>{name}</p>
       </div>
       <div className="post-note-input">
+      <p>{Counter}</p>
+
         <form onSubmit={handlePostNote} >
 
           <input 
