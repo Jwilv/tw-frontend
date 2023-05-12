9adb54ebb5fb23629aa80a943876c5bcff051f29
wilvers <juan.ignacio.wilvers@gmail.com>
2023-05-12 17:52:11 -0300

    arreglado

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,18 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { BasicLayout } from "../components/BasicLayout"
 import { MenuNotes } from "./MenuNotes"
 import { MenuRight } from "../components/MenuRight"
+import { useDispatch } from "react-redux"
+import { startUserData } from "../redux/user.slice"
 
 export const HomePage = () => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(startUserData())
+    }, [])
+    
     return (
         <div className="menu-container">
             <BasicLayout >
