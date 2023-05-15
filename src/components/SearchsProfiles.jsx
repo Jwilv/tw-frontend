6b7830b1de6a4fdd623b5de8e7117db75a47d794
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-05-15 15:26:19 -0300

    update store

--- src/components/SearchsProfiles.jsx
+++ src/components/SearchsProfiles.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { ProfileCard } from './ProfileCard'
+import { useSelector } from 'react-redux'
 
 export const SearchsProfiles = () => {
+
+    const {} = useSelector( state => state.listUsers)
     return (
         <div className="container-search-users">
             <ProfileCard />
--- src/redux/store.js
+++ src/redux/store.js
@@ -15,6 +15,6 @@ export const store = configureStore({
         user : userReducer,
         ProfileActive : profileReducer,
         notes : notesReducer,
-        listusers : listUsersReducer,
+        listUsers : listUsersReducer,
     }
 })
