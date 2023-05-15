d007294bbaf7fea63382822767fb403b77ec4411
wilvers <juan.ignacio.wilvers@gmail.com>
2023-05-15 18:06:59 -0300

    obtener datos en SearchUsersPage

--- src/pages/SearchUsersPage.jsx
+++ src/pages/SearchUsersPage.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NavbarLayout } from '../components/NavbarLayout'
 import { SearchMenu } from '../components/SearchMenu'
+import { useDispatch } from 'react-redux'
+import { startAddUsersInList } from '../redux/listUsers.slice'
 
 export const SearchUsersPage = () => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(startAddUsersInList(1, "follow"))
+    }, [])
+    
     return (
         <NavbarLayout>
             <SearchMenu />
