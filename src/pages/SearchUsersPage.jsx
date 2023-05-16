4fbfec9a0c6fae4abb06f786bf12137bcfa18738
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-05-16 15:01:07 -0300

    correction bug

--- src/pages/SearchUsersPage.jsx
+++ src/pages/SearchUsersPage.jsx
@@ -3,12 +3,14 @@ import { NavbarLayout } from '../components/NavbarLayout'
 import { SearchMenu } from '../components/SearchMenu'
 import { useDispatch } from 'react-redux'
 import { startAddUsersInList } from '../redux/listUsers.slice'
+import { openFollow } from '../redux/ui.slice'
 
 export const SearchUsersPage = () => {
 
     const dispatch = useDispatch()
 
     useEffect(() => {
+        dispatch(openFollow())
         dispatch(startAddUsersInList(1, "follow"))
     }, [])
     
