1d7e9e1e36a99538bcfeebb91ba38cd0791d6764
wilvers <juan.ignacio.wilvers@gmail.com>
2023-05-28 18:00:12 -0300

    handleUser

--- src/components/NotesUser.jsx
+++ src/components/NotesUser.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { ImgNoteUser } from './ImgNoteUser'
+import { useNavigate } from 'react-router'
 
 export const NotesUser = ({userId,message}) => {
+
+    const navigate = useNavigate()
+
+    const handleUser = ()=>{
+        navigate(`/profile/${userId}`)
+    }
+
     return (
         <div className='notes-user-container'>
 
-            <div className='notes-user-img'>
+            <div className='notes-user-img' onClick={handleUser}>
                 <ImgNoteUser id={userId}/>
             </div>
             <div className='notes-user-info'>
