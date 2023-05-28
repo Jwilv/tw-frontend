80f1417f8f5e757a40f7c5289b959d97f5e341a7
wilvers <juan.ignacio.wilvers@gmail.com>
2023-05-28 14:37:13 -0300

    update UserRecommended

--- src/components/UserRecommended.jsx
+++ src/components/UserRecommended.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
+import { ImgNoteUser } from './ImgNoteUser'
 
-export const UserRecommended = () => {
+export const UserRecommended = ({ id, name, surname }) => {
     return (
         <div className='menu-right-user'>
             <div className="menu-right-user-img">
-                <img
-                    src="https://tse2.mm.bing.net/th?id=OIP.eAevlGw1UR_UqXA_2y-2tAHaHa&pid=Api&P=0"
-                    alt="img user"
-                    className='img-user'
-                />
+                <ImgNoteUser id={id} />
             </div>
-            
+
             <div className="menu-right-user-name">
-                <p>juancito</p>
+                <p>{name} {surname}</p>
             </div>
         </div>
     )
